e1293dfa1493d2dc18739022d5c80d66705a816e
Samuli Nikkilä <sanikk@users.noreply.github.com>
2025-01-31 17:34:23 +0200

    landing page updates, accounts list page

--- routing/routes.py
+++ routing/routes.py
@@ -1,13 +1,29 @@
 from flask import render_template
 from app import app
-from db_module import get_last_restaurants, get_last_logged_in_accounts
+from db_module import (
+    get_last_restaurants,
+    get_last_logged_in_accounts,
+    get_last_events,
+    get_last_buffets,
+    get_last_ratings,
+)
 
 
 @app.route("/")
 def index():
     accounts = get_last_logged_in_accounts()
     restaurants = get_last_restaurants()
-    return render_template("index.html", restaurants=restaurants, accounts=accounts)
+    events = get_last_events()
+    buffets = get_last_buffets()
+    ratings = get_last_ratings()
+    return render_template(
+        "index.html",
+        restaurants=restaurants,
+        accounts=accounts,
+        events=events,
+        buffets=buffets,
+        ratings=ratings,
+    )
 
 
 @app.route("/accounts")
