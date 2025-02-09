0f6c5c4b92b58ae773a3f61c6b5ac2029abb36f5
Samuli Nikkilä <sanikk@users.noreply.github.com>
2025-02-10 00:59:03 +0200

    moved opening hours to their own tables
    updated schema & fill_db

--- schema.sql
+++ schema.sql
@@ -22,20 +22,6 @@ CREATE TABLE IF NOT EXISTS restaurants (
     account_id INTEGER,
     address TEXT,
     description TEXT,
-    mondaystart TEXT,
-    mondayend TEXT,
-    tuesdaystart TEXT,
-    tuesdayend TEXT,
-    wednesdaystart TEXT,
-    wednesdayend TEXT,
-    thursdaystart TEXT,
-    thursdayend TEXT,
-    fridaystart TEXT,
-    fridayend TEXT,
-    saturdaystart TEXT,
-    saturdayend TEXT,
-    sundaystart TEXT,
-    sundayend TEXT,
     FOREIGN KEY (account_id) REFERENCES accounts(id)
 );
 
@@ -80,22 +66,25 @@ CREATE TABLE IF NOT EXISTS buffets (
     posted_on TEXT,
     last_modified TEXT,
     name TEXT,
-    mondaystart TEXT,
-    mondayend TEXT,
-    tuesdaystart TEXT,
-    tuesdayend TEXT,
-    wednesdaystart TEXT,
-    wednesdayend TEXT,
-    thursdaystart TEXT,
-    thursdayend TEXT,
-    fridaystart TEXT,
-    fridayend TEXT,
-    saturdaystart TEXT,
-    saturdayend TEXT,
-    sundaystart TEXT,
-    sundayend TEXT,
     price INTEGER,
     description TEXT,
     FOREIGN KEY (restaurant_id) REFERENCES restaurants(id)
 );
 
+CREATE TABLE buffet_opening_hours (
+    weekday_number INTEGER,
+    buffet_id INTEGER,
+    buffet_open TEXT,
+    buffet_close TEXT,
+    FOREIGN KEY (buffet_id) REFERENCES buffets(id),
+    PRIMARY KEY (weekday_number, buffet_id)
+);
+
+CREATE TABLE restaurant_opening_hours (
+    weekday_number INTEGER,
+    restaurant_id INTEGER,
+    restaurant_open TEXT,
+    restaurant_close TEXT,
+    FOREIGN KEY (restaurant_id) REFERENCES restaurants(id),
+    PRIMARY KEY (weekday_number, restaurant_id)
+);
