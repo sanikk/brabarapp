79f1cfa056eb6d024d161c48519a61f3c8155c3d
Samuli Nikkilä <sanikk@users.noreply.github.com>
2025-02-10 01:19:35 +0200

    template filters
    named output_rating_stars properly
    added zip, just in case

--- app.py
+++ app.py
@@ -15,10 +15,15 @@ def inject_weekdays():
     return {"weekdays": day_name}
 
 
-@app.template_filter
+@app.template_filter("output_rating_stars")
 def output_rating_stars(rating: int, size: int = 30):
     star_icon = f'<img src="/static/icons8-star-50.png" alt="Star" style="width:{size}px;height:{size}px;" />'
     return Markup(star_icon * rating)
+
+
+@app.template_filter("zip")
+def zip_filter(a, b):
+    return zip(a, b)
 
 
 import routing.routes
